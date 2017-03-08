95b404cc5686f83ad3eb6db28c29eb94705ded4f
Bogdan Popov <popovbodya@gmail.com>
2017-03-08 18:44:15 +0300

    project added

---
+++ archiver/src/main/java/command/ZipExtractCommand.java
@@ -0,0 +1,30 @@
+package command;
+
+
+
+import exception.PathIsNotFoundException;
+import manager.ConsoleHelper;
+import manager.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ZipExtractCommand extends ZipCommand {
+    @Override
+    public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Распаковка архива.");
+
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите путь для распаковки:");
+            Path destinationPath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.extractAll(destinationPath);
+
+            ConsoleHelper.writeMessage("Архив был распакован.");
+
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Неверный путь для распаковки.");
+        }
+    }
+}
