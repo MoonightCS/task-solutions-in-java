ddb32f488b92cfb81ca4a59fd192662395f6b208
Bogdan Popov <popovbodya@gmail.com>
2017-03-08 18:44:54 +0300

    test class added

--- archiver/src/test/java/ArchiverTest.java
+++ archiver/src/test/java/ArchiverTest.java
@@ -1,3 +1,5 @@
+import manager.ZipFileManager;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
@@ -9,6 +11,8 @@ public class ArchiverTest {
     absolute path of archive -- /Users/bogdanpopov/Desktop/Archivator/archive.zip
     absolute path of file for archive -- /Users/bogdanpopov/Desktop/Archivator/file_for_test.txt
     absolute path of file2 for archive -- /Users/bogdanpopov/Desktop/Archivator/second_file_for_test.txt
+    absolute path of fodler for archive -- /Users/bogdanpopov/Desktop/Archivator/folder
+
      */
 
     public static void main(String[] args) throws Exception {
