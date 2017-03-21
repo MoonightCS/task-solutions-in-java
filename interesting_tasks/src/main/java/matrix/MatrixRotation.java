44aa839dad43f6adce6a0a46a85d97103b5c107a
Bogdan Popov <popovbodya@gmail.com>
2017-03-21 19:12:02 +0300

    marix rotation solution added

---
+++ interesting_tasks/src/main/java/matrix/MatrixRotation.java
@@ -0,0 +1,62 @@
+package matrix;
+
+public class MatrixRotation {
+    public static void main(String[] argv) {
+        int m = 6;
+        int[][] arr = new int[m][m];
+        fillMatrix(arr);
+        printArray(arr);
+        counterClockwiseRotate(arr);
+        clockwiseRotate(arr);
+        printArray(arr);
+    }
+
+    private static void counterClockwiseRotate(int[][] arr) {
+        int len = arr.length;
+        for (int i = 0; i < len / 2; i++) {
+            for (int j = i; j < len - 1 - i; j++) {
+                // меняем местами 4 угла
+                int tmp = arr[i][j];
+                arr[i][j] = arr[j][len - 1 - i];
+                arr[j][len - 1 - i] = arr[len - 1 - i][len - 1 - j];
+                arr[len - 1 - i][len - 1 - j] = arr[len - 1 - j][i];
+                arr[len - 1 - j][i] = tmp;
+            }
+        }
+    }
+
+    private static void clockwiseRotate(int[][] arr) {
+
+        int len = arr.length;
+        for (int i = 0; i < len / 2; i++) {
+            for (int j = len - 1 - i; j > i; j--) {
+                int tmp = arr[i][j];
+                arr[i][j] = arr[len - 1 - j][i];
+                arr[len - 1 - j][i] = arr[len - i - 1][len  - 1 - j];
+                arr[len - 1 - i][len - 1 - j] = arr[j][len - 1 - i];
+                arr[j][len - 1 - i] = tmp;
+            }
+        }
+
+    }
+
+
+    private static void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.printf("%3d ", arr[i][j]);
+            }
+            System.out.print("\n");
+        }
+        System.out.println();
+    }
+
+    private static void fillMatrix(int[][] arr) {
+        int len = arr.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                arr[i][j] = len * i + j;
+            }
+        }
+    }
+}
